93977214b5eed7573dadc28f64ae5483ffd7907f
bonbybao169 <63963202+bonbybao169@users.noreply.github.com>
2023-04-07 00:30:55 +0700

    Add firebase admin and Auth Guard

---
+++ apps/auth/src/app/commons/guard/auth.guard.ts
@@ -0,0 +1,31 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import admin from 'apps/auth/src/config/firebase';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization.split(' ')[1];
+
+    try {
+      const decodeValue = await admin.auth().verifyIdToken(token);
+
+      if (decodeValue) {
+        return true;
+      }
+    } catch (e) {
+      console.log(e);
+      if (e.code === 'auth/argument-error') {
+        throw new UnauthorizedException();
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -4,29 +4,16 @@
  */
 
 import { Logger } from '@nestjs/common';
-import { NestFactory, Reflector } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
-import { JwtAuthGuard } from './commons/guard/jwt.guard';
-import { RolesGuard } from './commons/guard/roles.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const reflector = app.get(Reflector);
   // const globalPrefix = 'api';
   // app.setGlobalPrefix(globalPrefix);
 
-  app.useGlobalGuards(new JwtAuthGuard(reflector), new RolesGuard(reflector));
-
-  const config = new DocumentBuilder()
-    .setTitle('AndroiSign')
-    .setDescription('AndroiSign API description')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  app.enableCors();
 
   const port = process.env.PORT || 3333;
   await app.listen(port);
