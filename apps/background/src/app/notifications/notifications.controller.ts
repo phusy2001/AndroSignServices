5d5f672b3d93e5981b04b930ba3b71015c4239d6
Nguyễn Ngọc Phú Sỹ <phusy9a11516@gmail.com>
2023-05-19 19:33:19 +0700

    add notification module in background service

--- apps/background/src/app/notifications/notifications.controller.ts
+++ apps/background/src/app/notifications/notifications.controller.ts
@@ -1,45 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { NotificationsService } from './notifications.service';
-import { CreateNotificationDto } from './dto/create-notification.dto';
-import { UpdateNotificationDto } from './dto/update-notification.dto';
 
 @Controller()
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
   @MessagePattern('createNotification')
-  sendToDevice(@Payload() data: any) {
-    // const message = {
-    //   data: {
-    //     score: '850',
-    //     time: '2:45',
-    //   },
-    //   token: registrationToken,
-    // };
-    // return this.notificationsService.create(createNotificationDto);
+  sendToDevice(@Payload() payload: any) {
+    const { data, token } = payload;
+    this.notificationsService
+      .send(data, token)
+      .then((response) => {
+        //
+      })
+      .catch((error) => {});
   }
 
-  @MessagePattern('findAllNotifications')
-  findAll() {
-    return this.notificationsService.findAll();
-  }
-
-  @MessagePattern('findOneNotification')
-  findOne(@Payload() id: number) {
-    return this.notificationsService.findOne(id);
-  }
-
-  @MessagePattern('updateNotification')
-  update(@Payload() updateNotificationDto: UpdateNotificationDto) {
-    return this.notificationsService.update(
-      updateNotificationDto.id,
-      updateNotificationDto
-    );
-  }
-
-  @MessagePattern('removeNotification')
-  remove(@Payload() id: number) {
-    return this.notificationsService.remove(id);
+  @Get('/:id')
+  findOne(@Payload() user_id: string) {
+    return this.notificationsService.findByUser(user_id);
   }
 }
--- apps/background/src/app/notifications/notifications.module.ts
+++ apps/background/src/app/notifications/notifications.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Notification,
+  NotificationSchema,
+} from './entities/notification.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Notification.name, schema: NotificationSchema },
+    ]),
+  ],
   controllers: [NotificationsController],
-  providers: [NotificationsService]
+  providers: [NotificationsService],
 })
 export class NotificationsModule {}
