9996c54575f8407df58f00843be1aba926a1386d
ekkob <ekkob@BLEU>
2023-06-17 21:17:05 +0700

    create eSign services

--- apps/esignature/src/app/app.module.ts
+++ apps/esignature/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpModule } from '@nestjs/axios';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 
@@ -11,6 +12,7 @@ import { ConfigModule } from '@nestjs/config';
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/?${process.env.MONGODB_OPTIONS}`
     ),
+    HttpModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- apps/esignature/src/app/app.service.ts
+++ apps/esignature/src/app/app.service.ts
@@ -1,8 +1,47 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-
+import * as https from 'https';
 @Injectable()
 export class AppService {
+  constructor(private readonly httpService: HttpService) {
+    this.httpsAgent = new https.Agent({
+      rejectUnauthorized: false,
+    });
+    this.certHost = 'localhost:4200/api/Cer/';
+  }
+  private certHost: string;
+  private httpsAgent: https.Agent;
+
   getData(): { message: string } {
     return { message: 'Welcome to esignature!' };
+  }
+
+  test() {
+    this.httpService
+      .post(this.certHost, {
+        httpsAgent: this.httpsAgent,
+      })
+      .subscribe((res) => {
+        return res.data;
+      });
+  }
+
+  createSelfCA(
+    issued: string,
+    password: string,
+    fileName: string,
+    expireAfter?: number
+  ) {
+    this.httpService
+      .post(
+        this.certHost + 'CreateSelfCA',
+        [issued, password, fileName, expireAfter],
+        {
+          httpsAgent: this.httpsAgent,
+        }
+      )
+      .subscribe((res) => {
+        return res.data;
+      });
   }
 }
