5e611194a5c683f52913e139a5a2b5a97514a2d4
bonbybao169 <63963202+bonbybao169@users.noreply.github.com>
2023-07-11 09:44:55 +0700

    remove unnecessary log

--- apps/user/src/users/users.controller.ts
+++ apps/user/src/users/users.controller.ts
@@ -214,7 +214,6 @@ export class UsersController {
   @Put('/:id/createUserCa')
   async createUserCa(@Param('id') uid: string, @Body() dto) {
     try {
-      console.log('dto', dto);
       const { email, passwordCa, expireAfter } = dto;
 
       const user = await this.usersService.createUserCa(
